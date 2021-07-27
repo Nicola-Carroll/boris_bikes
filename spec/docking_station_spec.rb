67e0256887d54282abd9e5e3ed27779b62ec01c2
nicola <nicolaccarroll@gmail.com>
2021-07-27 15:52:43 +0100

    docking_station responds to release_bike test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,9 @@
 require "docking_station"
 
 describe DockingStation do
+  subject(:docking_station) { described_class.new }
+
+  it "should respond to release bike" do
+    expect(:docking_station).to respond_to(:release_bike)
+  end
 end
